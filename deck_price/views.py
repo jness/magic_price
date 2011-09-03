99b7a0878919f3a1564e74e31ad832b765f5dcd7
Jeffrey Ness <jness@flip-edesign.com>
2011-09-03 11:35:41 -0500

    adding bitlyapi shorturls

--- deck_price/views.py
+++ deck_price/views.py
@@ -1,56 +1,66 @@
 # Create your views here.
 
-from django.http import HttpResponse, Http404
+from django.http import HttpRequest, Http404
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.conf import settings
 
+import bitlyapi
 import urllib
 from urllib2 import urlopen, quote, Request
 from re import compile, search
 
 def index(request):
-    if request.method == 'POST':
-        total_cost = []
-        data = request.POST['deck']
-        results = []
-        if data:
-            for line in data.split('\n'):
-                # grab the first element and check its a int
-                c = search('^\d*', line)
-                if c:
-                    count = c.group(0)
-                else:
-                    raise Http404
-
-                # pull card name
-                card = ' '.join(line.split()[1:])
-                if not card:
-                    continue
-               
-                # try to get prices fro TCGPlayer
-                quoted_card = quote(card)
-                u = urlopen('http://magic.tcgplayer.com/db/wp-ch.asp?CN=%s' % quoted_card)
-                try:
-                    prices = compile('\$(\d*.\d\d)\r\n[\t]*</div>').findall(u.read())
-                    lowest_price = prices[2]
-                except IndexError:
-                    message = 'Unable to get card <b>%s</b>, please check spelling' % card
-                    return render(request, 'index.html', {'error': message, 'data': data}) 
-                results.append({'name': card, 
-                                'count': count, 
-                                'price_each': lowest_price, 
-                                'price': '%.2f' % (float(lowest_price) * int(count))})
-                total_cost.append('%.2f' % (float(lowest_price) * int(count)))
-
-            deck_total = ('%.2f' % (sum(float(i) for i in total_cost)))
-            return render(request, 'list.html', {'results': results, 'total': deck_total})
-
-        else:
-            raise Http404
-
-    else:
+    try:
+        data = request.GET['deck']
+    except:
         return render(request, 'index.html')
 
+    total_cost = []
+    data = request.GET['deck']
+    results = []
+    if data:
+        for line in data.split('\n'):
+            # grab the first element and check its a int
+            c = search('^\d*', line)
+            if c:
+                count = c.group(0)
+            else:
+                raise Http404
+
+            # pull card name
+            card = ' '.join(line.split()[1:])
+            if not card:
+                continue
+
+            # try to get prices fro TCGPlayer
+            quoted_card = quote(card)
+            u = urlopen('http://magic.tcgplayer.com/db/wp-ch.asp?CN=%s' % quoted_card)
+            try:
+                prices = compile('\$(\d*.\d\d)\r\n[\t]*</div>').findall(u.read())
+                lowest_price = prices[2]
+            except IndexError:
+                message = 'Unable to get card <b>%s</b>, please check spelling' % card
+                return render(request, 'index.html', {'error': message, 'data': data}) 
+            results.append({'name': card, 
+                            'count': count, 
+                            'price_each': lowest_price, 
+                            'price': '%.2f' % (float(lowest_price) * int(count))})
+            total_cost.append('%.2f' % (float(lowest_price) * int(count)))
+
+        deck_total = ('%.2f' % (sum(float(i) for i in total_cost)))
+
+        # create bit.ly short link
+        url = 'http://%s%s' % (request.META['HTTP_HOST'], HttpRequest.get_full_path(request))
+        api_user = settings.BITLY_API_USER
+        api_key = settings.BITLY_API_KEY
+        b = bitlyapi.BitLy(api_user, api_key)
+        res = b.shorten(longUrl='%s' % url)
+
+        return render(request, 'list.html', {'results': results, 'total': deck_total, 'short': res['url']})
+
+    else:
+        raise Http404
 
 def gatherer_lookup(request, card):
     page = urlopen('http://gatherer.wizards.com/Pages/Default.aspx').read()
