ff2e8bfc5e21133436bbefc7b6ded955e2e674f9
Jeffrey Ness <jness@flip-edesign.com>
2011-09-03 16:48:06 -0500

    adding some images

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -8,3 +9,5 @@ urlpatterns = patterns('',
     url(r'^$', 'deck_price.views.index'),
     url(r'^gatherer/(.*)$', 'deck_price.views.gatherer_lookup'),
 )
+
+urlpatterns += staticfiles_urlpatterns()
